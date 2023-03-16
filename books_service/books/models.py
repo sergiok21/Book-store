11e6b35c64cfed4d6553dcf3f09cfcdaf529f419
Sergey <kulibseryyy@gmail.com>
2023-03-16 18:58:51 +0200

    Book models

---
+++ books_service/books/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+
+class BookCategory(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    description = models.TextField(null=True, blank=True)
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return f'Name: {self.name}'
+
+
+class Book(models.Model):
+    name = models.CharField(max_length=256, unique=True)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=0)
+    image = models.ImageField(upload_to='books_images', null=True, blank=True)
+    category = models.ForeignKey(to=BookCategory, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Book'
+        verbose_name_plural = 'Books'
+        ordering = ['id']
+
+    def __str__(self):
+        return f'Продукт: {self.name} | Категория: {self.category.name}'
