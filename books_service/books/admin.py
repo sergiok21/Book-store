f8a136a8d4cea91837d2b46331c751d002895d3a
Sergey <kulibseryyy@gmail.com>
2023-03-16 19:31:48 +0200

    Book admin

--- books_service/books/admin.py
+++ books_service/books/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from books.models import Book, BookCategory
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price', 'quantity', 'category']
+    fields = ['name', 'description', ('price', 'quantity'), 'image', 'category']
+    readonly_fields = ['description']
+    search_fields = ['name', 'price']
+    search_help_text = 'Name | Price'
+    ordering = ['name']
+
+
+@admin.register(BookCategory)
+class BookCategoryAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    fields = ['name', 'description']
+    search_fields = ['name']
+    search_help_text = 'Name'
