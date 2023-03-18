cae71e663d27f0a7c388b586cb3b01bd3c05c6e7
Sergey <kulibseryyy@gmail.com>
2023-03-18 15:00:33 +0200

    Shop page

--- books_service/books/urls.py
+++ books_service/books/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
-from books.views import IndexView, ContactListView
+from books.views import IndexView, ContactCreateView, BooksListView
 
 app_name = 'books'
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
-    path('contact/', ContactListView.as_view(), name='contact'),
+    path('shop/<str:category>-books/', BooksListView.as_view(), name='shop'),
+    path('shop/<str:category>-books/<int:page>/', BooksListView.as_view(), name='paginator'),
+    path('shop/view/<str:name>/', BooksListView.as_view(), name='view'),
+    path('contact/', ContactCreateView.as_view(), name='contact'),
 ]
--- books_service/books/views.py
+++ books_service/books/views.py
@@ -1,8 +1,9 @@
+from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse, reverse_lazy
 from django.views.generic import TemplateView, ListView, CreateView
 
 from books.forms import MessageForm
-from books.models import Contact, Message
+from books.models import Contact, Message, Book
 from common.views import TitleMixin
 
 
@@ -11,7 +12,7 @@ class IndexView(TitleMixin, TemplateView):
     title = 'Book Store'
 
 
-class ContactListView(TitleMixin, CreateView):
+class ContactCreateView(TitleMixin, CreateView):
     model = Contact
     template_name = 'books/contact.html'
     title = 'Book Store - Contact'
@@ -24,3 +25,21 @@ class ContactListView(TitleMixin, CreateView):
 
     def form_valid(self, form):
         return super().form_valid(form)
+
+
+class BooksListView(TitleMixin, ListView):
+    model = Book
+    template_name = 'books/shop.html'
+    title = 'Book Store - Products'
+    context_object_name = 'books'
+    paginate_by = 6
+
+    def get(self, request, *args, **kwargs):
+        category = kwargs.get('category')
+        self.extra_context = {'category': category}
+        if category == 'all':
+            self.queryset = Book.objects.all()
+            return super().get(request, *args, **kwargs)
+        else:
+            self.queryset = Book.objects.filter(category__name__icontains=category)
+            return super().get(request, *args, **kwargs)
