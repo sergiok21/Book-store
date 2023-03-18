fa71d379e241ec66453c0e4e897c2af48372ac13
Sergey <kulibseryyy@gmail.com>
2023-03-18 22:26:11 +0200

    User and Email models

---
+++ users_service/users/models.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    is_verified_email = models.BooleanField(default=False)
+
+
+class EmailVerification(models.Model):
+    code = models.UUIDField(unique=True)
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    expiration = models.DateTimeField()
+
+    def __str__(self):
+        return f'Email verification object for {self.user.email}'
