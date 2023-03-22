62ea6732afac2678e9d69989820be9084e8a7917
Sergey <kulibseryyy@gmail.com>
2023-03-22 17:42:54 +0200

    Update common book service

--- books_service/common/views.py
+++ books_service/common/views.py
@@ -1,15 +1,6 @@
 from rest_framework.permissions import AllowAny, IsAuthenticatedOrReadOnly
 
 
-class TitleMixin:
-    title = None
-
-    def get_context_data(self, **kwargs):
-        context = super(TitleMixin, self).get_context_data(**kwargs)
-        context['title'] = self.title
-        return context
-
-
 class PermissionMixin:
     def get_permissions(self):
         if self.request.user.is_superuser:
