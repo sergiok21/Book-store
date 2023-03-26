20bdaf3842a625c4be6c79f126b70275acce848b
Sergey <kulibseryyy@gmail.com>
2023-03-26 22:35:57 +0300

    Basket model

--- baskets_service/baskets/models.py
+++ baskets_service/baskets/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class BasketQuerySet(models.QuerySet):
+    def total_sum(self):
+        return sum(basket.sum() for basket in self)
+
+    def total_quantity(self):
+        return sum(basket.quantity for basket in self)
+
+
+class Basket(models.Model):
+    user_id = models.PositiveIntegerField(default=0)
+    product_id = models.PositiveIntegerField(default=0)
+    name = models.CharField(max_length=256, unique=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    quantity = models.PositiveSmallIntegerField(default=0)
+    message = models.TextField()
+    created_timestamp = models.DateTimeField(auto_now_add=True)
+
+    objects = BasketQuerySet.as_manager()
--- baskets_service/baskets_service/settings.py
+++ baskets_service/baskets_service/settings.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar',
+    'django_extensions',
+    'rest_framework',
 
     'baskets',
 ]
@@ -51,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'baskets_service.urls'
